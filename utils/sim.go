155078ca2aacef1baf77ac0511de7bf2aef2d4b2
alialsaffarcodexals <justforcodexals@gmail.com>
2025-07-29 12:53:06 -0700

    Refactor with helper functions

--- utils/sim.go
+++ utils/sim.go
@@ -1,11 +1,5 @@
 package utils
 
-import (
-	"fmt"
-	"sort"
-	"strings"
-)
-
 type antState struct {
 	id   int
 	path int
@@ -27,57 +21,10 @@ func SimulateMulti(g *Graph, paths [][]*Room) []string {
 	finished := 0
 	var moves []string
 	for finished < len(route) {
-		type evt struct {
-			id   int
-			room *Room
-		}
-		var evts []evt
-		for id := 0; id < len(route); id++ {
-			if !started[id] {
-				continue
-			}
-			p := paths[route[id]]
-			if pos[id] < len(p)-1 {
-				next := p[pos[id]+1]
-				if next == g.End || occupancy[next] == 0 {
-					if p[pos[id]] != g.Start {
-						delete(occupancy, p[pos[id]])
-					}
-					pos[id]++
-					if next != g.End {
-						occupancy[next] = id + 1
-					} else {
-						finished++
-					}
-					evts = append(evts, evt{id: id + 1, room: next})
-				}
-			}
-		}
-		for i, q := range queues {
-			if len(q) == 0 {
-				continue
-			}
-			ant := q[0]
-			next := paths[i][1]
-			if next == g.End || occupancy[next] == 0 {
-				started[ant] = true
-				pos[ant] = 1
-				if next != g.End {
-					occupancy[next] = ant + 1
-				} else {
-					finished++
-				}
-				queues[i] = q[1:]
-				evts = append(evts, evt{id: ant + 1, room: next})
-			}
-		}
+		evts := moveStartedAnts(g, paths, pos, started, occupancy, &finished, route)
+		evts = append(evts, startQueuedAnts(g, paths, queues, started, pos, occupancy, &finished)...)
 		if len(evts) > 0 {
-			sort.Slice(evts, func(i, j int) bool { return evts[i].id < evts[j].id })
-			line := make([]string, len(evts))
-			for i, e := range evts {
-				line[i] = fmt.Sprintf("L%d-%s", e.id, e.room.Name)
-			}
-			moves = append(moves, strings.Join(line, " "))
+			moves = append(moves, formatEvents(evts))
 		}
 	}
 	return moves
